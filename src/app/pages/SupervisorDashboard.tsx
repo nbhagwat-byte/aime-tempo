72d30d5e3ed1824b78f465eb83e0590cced76bfb
nbhagwat-byte <nbhagwat@goapgtech.com>
2026-03-11 18:38:32 -0400

    Update SupervisorDashboard.tsx

--- src/app/pages/SupervisorDashboard.tsx
+++ src/app/pages/SupervisorDashboard.tsx
@@ -9,7 +9,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/app/components/ui/ta
 import { Badge } from '@/app/components/ui/badge';
 import { ProjectManagement } from '@/app/components/supervisor/ProjectManagement';
 import { TeamManagement } from '@/app/components/supervisor/TeamManagement';
-import { TimeReview } from '@/app/components/supervisor/TimeReview';
+import { TimeReview } from '@/app/components/supervisor/TimeLogs';
 import { PayrollExport } from '@/app/components/supervisor/PayrollExport';
 import { PendingProjectsQueue } from '@/app/components/supervisor/PendingProjectsQueue';
 
@@ -94,3 +94,4 @@ export default function SupervisorDashboard() {
     </div>
   );
 }
+
